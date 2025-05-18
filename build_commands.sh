76d7dbc4b6f5380376df84c2dca8a04fbc7b266d
nati <nwtbekele@gmail.com>
2025-05-18 04:29:35 -0500

    Include option to build only provided list of commands

--- build_commands.sh
+++ build_commands.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+if [ $# -ne 0 ]; then
+    entries=$@
+else
+    entries=*
+fi
+
 # run in the project directory
-for entry in *; do
+for entry in $entries; do
     if [ -d "$entry" ] && [[ $entry == m* ]]; then
         src_code="$PWD/"$entry"/"$entry".cpp"
-        (cd bin && g++ -o "$entry" "$src_code")
+        (cd bin && g++ -o "$entry" "$src_code" && echo $entry built)
     fi
 done
